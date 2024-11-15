700b1d125d60574b524ca07aa5fb026d50abe8f8
Samuel Leite <samuel-leite@outlook.com.br>
2024-11-15 16:57:54 -0300

    feat(all): ✨ add dynamic browsrer and device
    add configuracao pra deixar dinamico a escolha do navegador e
    dispositivo

---
+++ helpers/browsers.js
@@ -0,0 +1,36 @@
+const { devices } = require('@playwright/test');
+
+class BrowserConfig {
+  constructor() {
+    this.browsers = {
+      chromium: 'chromium',
+      firefox: 'firefox',
+      webkit: 'webkit',
+      edge: 'msedge',
+    };
+
+    this.devicesConfig = {
+      'Desktop Chrome': devices['Desktop Chrome'],
+      'iPad': devices['iPad'],
+      'Pixel 5': devices['Pixel 5'],
+    };
+  }
+
+  getBrowsers() {
+    return this.browsers;
+  }
+
+  getDevicesConfig() {
+    return this.devicesConfig;
+  }
+
+  getBrowser(browserName) {
+    return this.browsers[browserName] || this.browsers.chromium;
+  }
+
+  getDeviceConfig(deviceName) {
+    return this.devicesConfig[deviceName] || this.devicesConfig['Desktop Chrome'];
+  }
+}
+
+module.exports = new BrowserConfig();
