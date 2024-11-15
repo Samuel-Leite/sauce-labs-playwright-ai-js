e4a2f0668a1d04588293b6859cf8ea1d043952a2
Samuel Leite <samuel-leite@outlook.com.br>
2024-11-15 16:41:41 -0300

    feat(all): ✨ add allure preport

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
   // /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   // /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['allure-playwright']],
   use: {
     // browserName: 'chromium',
     headless: true,
